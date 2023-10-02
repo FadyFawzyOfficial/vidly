bfc5a7e57f7f822c4d17295981436a84df104b74
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-02 12:50:40 +0300

    4. Validate genres that we are getting in the request before attempting to update the database

--- routes/genres.js
+++ routes/genres.js
@@ -46,8 +46,13 @@ router.get("/:id", async (request, response) => {
 });
 
 router.put("/:id", async (request, response) => {
-  // 1. Look out the genre
-  // If not exist, return 404
+  //! We need to validate this genre that we are getting in the request before
+  //! attempting to update the database.
+  // 1. Validate
+  // If invalid, return 400 - Bad Request
+  const { error } = validateGenre(request.body);
+  if (error) return response.status(400).send(error.details[0].message);
+
   const genre = await Genre.findByIdAndUpdate(
     request.params.id,
     {
@@ -59,23 +64,14 @@ router.put("/:id", async (request, response) => {
     { new: true }
   );
 
+  // 2. Look out the genre
+  // If not exist, return 404
   if (!genre)
     return response
       .status(404)
       .send("The genre with the given id was not found");
 
-  // 2. Validate
-  // If invalid, return 400 - Bad Request
-  // const { error } = validateGenre(request.body);
-
-  // if (error) return response.status(400).send(error.details[0].message);
-
-  // 3. Update Genre
-  // Return the updated Genre
-  // genre.name = request.body.name;
-
-  // genre = await genre.save();
-
+  //* The genre we got here is the updated genre
   response.send(genre);
 });
 
