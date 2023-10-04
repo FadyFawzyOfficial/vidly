d15ce5adc9b82f06ade9f24bb13a0c216330fa53
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-04 10:58:49 +0300

    4. Build Post (CREATE) Method to post (create) customer & save it on MongoDB

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const express = require("express");
+const { route } = require("./genres");
 const router = express.Router();
 
 const customerSchema = new mongoose.Schema({
@@ -35,5 +36,31 @@ router.get("/:id", async (request, response) => {
 
   response.send(customer);
 });
+
+router.post("/", async (request, response) => {
+  const { error } = validateCustomer(request.body);
+
+  if (error) return response.status(400).send(error.details[0].message);
+
+  //! Create an Object based on Customer Class Model
+  let customer = new Customer({
+    name: request.body.name,
+    phone: request.body.phone,
+    isGold: request.body.isGold,
+  });
+
+  //! Save a Model (Customer) Data as a Document to MongoDB
+  customer = await customer.save();
+
+  //* Return the saved customer to the client with its created id.
+  response.send(customer);
+});
+
+function validateCustomer(customer) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+}
 
 module.exports = router;
