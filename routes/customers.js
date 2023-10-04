95cb497af100a4d539b556157e4bd4d8d362e6af
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-04 10:50:52 +0300

    3. Build Get (READ) Methods to get (read) customers or a specified one from Database

--- routes/customers.js
+++ routes/customers.js
@@ -20,4 +20,20 @@ const customerSchema = new mongoose.Schema({
 //! Compile Customer Schema into a Customer Model (Class)
 const Customer = mongoose.model("Customer", customerSchema);
 
+router.get("/", async (request, response) => {
+  const customers = await Customer.find().sort("name");
+  response.send(customers);
+});
+
+router.get("/:id", async (request, response) => {
+  const customer = await Customer.findById(request.params.id);
+
+  if (!customer)
+    return response
+      .status(404)
+      .send("The Customer with the given id was not found");
+
+  response.send(customer);
+});
+
 module.exports = router;
