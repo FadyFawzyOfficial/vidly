4dddad25f7c872ad832769e07362efb11d857165
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-04 10:46:45 +0300

    2. Define the CustomerSchema & Compile it into Customer Model (Class)

--- routes/customers.js
+++ routes/customers.js
@@ -3,4 +3,21 @@ const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isGold: Boolean,
+});
+
+//! Compile Customer Schema into a Customer Model (Class)
+const Customer = mongoose.model("Customer", customerSchema);
+
 module.exports = router;
